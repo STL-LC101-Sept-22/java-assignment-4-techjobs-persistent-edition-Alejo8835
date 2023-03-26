4ea4b2c4ba8814a98d02d85105355164fa877f1f
Alejandro Pedreros <pedrerosalejandro_21@hotmail.com>
2023-03-26 11:36:27 -0500

    Connect MySQL to the Spring application and add annotations to the AbstractEntity Class.

--- queries.sql
+++ queries.sql
@@ -1,4 +1,10 @@
 -- Part 1: Test it with SQL
+    --    CREATE TABLE job (
+    --         id INT,
+    --         employer VARCHAR,
+    --         name VARCHAR,
+    --         skills
+    --    );
 
 -- Part 2: Test it with SQL
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String location;
 
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    
 }
