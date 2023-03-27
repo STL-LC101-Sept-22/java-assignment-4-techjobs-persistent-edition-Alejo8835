5b8d21b50326f8ebf9b5e7733af0e43a798aabe1
Alejandro Pedreros <pedrerosalejandro_21@hotmail.com>
2023-03-27 17:00:01 -0500

    Finish updating the SkillController class.

--- src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
@@ -5,8 +5,11 @@ import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("skills")
@@ -22,10 +25,35 @@ public class SkillController {
         return "skills/index";
     }
 
-    @GetMapping
+    @GetMapping("add")
     public String displayAddSkillForm(Model model) {
         model.addAttribute(new Skill());
         return "skills/add";
+        }
+
+    @PostMapping("add")
+    public String processAddSkillForm(@ModelAttribute @Valid Skill newSkill,
+                                      Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            return "skills/add";
+        }
+
+        skillRepository.save(newSkill);
+        return "redirect:";
+    }
+
+    @GetMapping("view/{skillId}")
+    public String displayViewSkill(Model model, @PathVariable int skillId) {
+
+            Optional optSkill = skillRepository.findById(skillId);
+            if (optSkill.isPresent()) {
+                Skill skill = (Skill) optSkill.get();
+                model.addAttribute("skill", skill);
+                return "skills/view";
+            } else {
+                return "redirect:../";
+
         }
     }
 
